b4dd675a120c5974ea18e5c0774da6f00ef24cbd
Bremer31 <97674557+Bremer31@users.noreply.github.com>
2022-03-06 13:41:26 -0500

    Create mac_changer.py
    this code can change your mac address.

---
+++ mac_changer.py
@@ -0,0 +1,28 @@
+import subprocess
+import optparse
+import re
+def getting_input():
+    object_parse = optparse.OptionParser()
+    object_parse.add_option("-i", "--interface", dest="interface", help="interface to change")
+    object_parse.add_option("-m", "--mac", dest="mac_address", help="type your new mac address")
+    return object_parse.parse_args()
+
+def change_mac(user_interface,user_mac):
+    subprocess.call(["ifconfig", user_interface, "down"])
+    subprocess.call(["ifconfig", user_interface, "hw", "ether", user_mac])
+    subprocess.call(["ifconfig", user_interface, "up"])
+def control_new_mac(interface):
+    ifconfig = subprocess.check_output(["ifconfig",interface])
+    new_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",str(ifconfig))
+    if new_mac:
+        return(new_mac.group(0))
+    else:
+        return None
+(user_input,arguements) = getting_input()
+print("mac changer started")
+change_mac(user_input.interface,user_input.mac_address)
+finalized_mac = control_new_mac(str(user_input.interface))
+if finalized_mac == user_input.mac_address:
+    print("Success")
+else:
+    print("error")
