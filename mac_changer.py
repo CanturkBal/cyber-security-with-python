0e2e76a67a1586ebc06ad58a141b5335203e81ed
Bremer31 <97674557+Bremer31@users.noreply.github.com>
2022-03-06 13:49:24 -0500

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -6,11 +6,13 @@ def getting_input():
     object_parse.add_option("-i", "--interface", dest="interface", help="interface to change")
     object_parse.add_option("-m", "--mac", dest="mac_address", help="type your new mac address")
     return object_parse.parse_args()
+#creating the inputs to change your mac address with optparse
 
 def change_mac(user_interface,user_mac):
     subprocess.call(["ifconfig", user_interface, "down"])
     subprocess.call(["ifconfig", user_interface, "hw", "ether", user_mac])
     subprocess.call(["ifconfig", user_interface, "up"])
+# using subprocces library to write some commands to change the mac address
 def control_new_mac(interface):
     ifconfig = subprocess.check_output(["ifconfig",interface])
     new_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",str(ifconfig))
@@ -18,6 +20,7 @@ def control_new_mac(interface):
         return(new_mac.group(0))
     else:
         return None
+#checking the new mac if its changed with re library
 (user_input,arguements) = getting_input()
 print("mac changer started")
 change_mac(user_input.interface,user_input.mac_address)
