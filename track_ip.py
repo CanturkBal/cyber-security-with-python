76df7a5adf9b4e5b936d554529a83667728205e0
Bremer31 <97674557+Bremer31@users.noreply.github.com>
2022-05-03 03:28:35 +0300

    it trackes the ip address and gives you the cordinates.
    it saves the result to a html file if you want to look at it. Open from a browser

---
+++ track_ip.py
@@ -0,0 +1,24 @@
+import geocoder
+import folium
+import codecs
+import optparse
+def getting_input():
+    object_parse = optparse.OptionParser()
+    object_parse.add_option("-i", "--ip", dest="ip", help="ip to change,if you want to search your ip, just type `me`")
+    return object_parse.parse_args()
+[user_input,arguements] = getting_input()  
+try:
+
+    g = geocoder.ip(user_input.ip) #==> gets your ip address or put an ip address
+
+    myaddress = g.latlng
+
+    print(myaddress)
+    my_map1 = folium.Map(location=myaddress,zoom_start=12)
+    folium.CircleMarker(location=myaddress,radius=50,popup="Yorkshire").add_to(my_map1)
+
+    folium.Marker(location=myaddress,popup="Yorkshire").add_to(my_map1)
+
+    my_map1.save("my_map.html")
+except:
+    print("can not find the location of your IP!!")
