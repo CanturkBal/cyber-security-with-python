391813b51797ce69e7072c9f255879ebcbd58b55
Bremer31 <97674557+Bremer31@users.noreply.github.com>
2022-05-03 03:30:35 +0300

    Update track_ip.py

--- track_ip.py
+++ track_ip.py
@@ -2,17 +2,25 @@ import geocoder
 import folium
 import codecs
 import optparse
+import pyfiglet
+import time
 def getting_input():
     object_parse = optparse.OptionParser()
     object_parse.add_option("-i", "--ip", dest="ip", help="ip to change,if you want to search your ip, just type `me`")
     return object_parse.parse_args()
 [user_input,arguements] = getting_input()  
+
+out = pyfiglet.figlet_format("IP TRACKER")
+
+
 try:
 
     g = geocoder.ip(user_input.ip) #==> gets your ip address or put an ip address
 
     myaddress = g.latlng
-
+    print(out)
+    time.sleep(1)
+    
     print(myaddress)
     my_map1 = folium.Map(location=myaddress,zoom_start=12)
     folium.CircleMarker(location=myaddress,radius=50,popup="Yorkshire").add_to(my_map1)
